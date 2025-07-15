93626eee02a30c1b1c2b253c0181494b54fa1c77
Fazo01 <fg7829098@gmail.com>
2025-07-15 14:26:48 +0500

    Lecture 12 complete

--- Practice set/routes/Hostrouter.js
+++ Practice set/routes/Hostrouter.js
@@ -4,19 +4,14 @@ const rootDir = require("../utils/pathutils");
 const hostrouter = express.Router();
 hostrouter.get("/add-home", (req, res, next) => {
   console.log(req.url, req.method);
-  res.render("addHome", { pageTitle: "Add Home" }); //Important to change in partial
+  res.render("addHome", { pageTitle: "Add Home",currentPage:'addHome' }); //Important to change in partial
 });
 const registeredHomes = [];//to store input data
 
 hostrouter.post("/add-home", (req, res, next) => {
-  console.log(req.body, req.body.houseName); //parcel
-  registeredHomes.push({
-    houseName: req.body.houseName,
-    price_per_night: req.body.price_per_night,
-    location: req.body.location,
-    pic:req.body.location,
-  });
-  res.render("homeadd", { pageTitle: "Home Add" }); //Important to change in partial
+  console.log(req.body); //parcel
+  registeredHomes.push(req.body);
+  res.render("homeadd", { pageTitle: "Home Add",currentPage:'HomeAdded' }); //Important to change in partial
 });
 exports.hostrouter = hostrouter;
 exports.registeredHomes = registeredHomes;

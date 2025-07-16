1fa0eea9e980a802ff50766afbb7f916b72fe7e5
Fazo01 <fg7829098@gmail.com>
2025-07-16 18:33:21 +0500

    Angela Yu 4.2 complete

--- 4.2+Passing+Data/index.js
+++ 4.2+Passing+Data/index.js
@@ -6,10 +6,15 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs")
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const ans=req.body["fName"].length+req.body["lName"].length;
+  res.render("index.ejs",{numberOfLetters:ans})
+});
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port http://localhost:${port}`);
 });
--- 4.2+Passing+Data/solution.js
+++ 4.2+Passing+Data/solution.js
@@ -16,5 +16,5 @@ app.post("/submit", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port http://localhost:${port}`);
 });

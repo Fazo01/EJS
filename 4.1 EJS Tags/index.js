6ca0759f90b9d8a389917ed7ff062b41ab1c488b
Fazo01 <fg7829098@gmail.com>
2025-07-16 15:05:36 +0500

    AngelaYu 4.2 Initialize

--- 4.1 EJS Tags/index.js
+++ 4.1 EJS Tags/index.js
@@ -1,14 +1,14 @@
 import express from "express";
-import {fileURLToPath} from "url";//create a path
-import path from "path";//create a dirname
+// import {fileURLToPath} from "url";//create a path
+// import path from "path";//create a dirname
 const app = express();
 const port = 3000;
 
-const __filename=fileURLToPath(import.meta.url)//create a path
-const __dirname=path.dirname(__filename)
+// const __filename=fileURLToPath(import.meta.url)//create a path
+// const __dirname=path.dirname(__filename)
 
-app.set("view engine", "ejs");
-app.set("views",path.join(__dirname,"views"));
+// app.set("view engine", "ejs");
+// app.set("views",path.join(__dirname,"views"));
 
 
 app.get("/", (req, res) => {
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
     items: ["apple", "banana", "cherry"],
     htmlContent: "<strong>This is some strong text</strong>",//<%- <h1>Hello</h1>%>    Render HTML we have element in this object
   };
-  res.render("index", data);
+  res.render("index.ejs", data);
 });
 
 app.listen(port, () => {

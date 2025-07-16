74a769e2dea51de1d07d4a896a5a645fbeee1b5b
Fazo01 <fg7829098@gmail.com>
2025-07-16 11:40:29 +0500

    Angela _02 EJS Tags

--- 4.1 EJS Tags/index.js
+++ 4.1 EJS Tags/index.js
@@ -1,17 +1,26 @@
 import express from "express";
+import {fileURLToPath} from "url";//create a path
+import path from "path";//create a dirname
 const app = express();
 const port = 3000;
+
+const __filename=fileURLToPath(import.meta.url)//create a path
+const __dirname=path.dirname(__filename)
+
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname,"views"));
+
 
 app.get("/", (req, res) => {
   const data = {
     title: "EJS Tags",
     seconds: new Date().getSeconds(),
     items: ["apple", "banana", "cherry"],
-    htmlContent: "<strong>This is some strong text</strong>",
+    htmlContent: "<strong>This is some strong text</strong>",//<%- <h1>Hello</h1>%>    Render HTML we have element in this object
   };
-  res.render("index.ejs", data);
+  res.render("index", data);
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port http://localhost:${port}`);
 });
